c56b62ac531d50cf6c281b921a2a396424d27246
rieg-ec <rieg@uc.cl>
2020-11-01 12:41:27 -0300

    fix title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col w-full">
     <div class="py-20 text-center text-4xl">
-      GUESS WHO
+        NUNCA NUNCA...
     </div>
 
     <div class="flex flex-col">
