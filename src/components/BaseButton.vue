7c385870e95f67a73e1a28b4f6601296bd943f6c
rieg-ec <rieg@uc.cl>
2020-11-01 12:41:11 -0300

    improve button style

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -2,17 +2,19 @@
   <div class="outline-none">
     <button type="button"
     :disabled="disabled || loading"
-    @click="$emit('clicked')"
-    class="bg-deep-purple-400-accent text-white font-mono
-          py-2 px-6 flex justify-center items-center rounded
+    @click="clickEvent"
+    class="bg-deep-purple-200-accent text-deep-purple-400-accent-contrast font-mono
+          py-2 px-6 rounded text-center overflow-hidden
           shadow-2xl focus:outline-none"
 
-    :class="[$style.button,
-            { 'transition duration-200 hover:bg-purple-400': !loading && !disabled,
-              'transform active:translate-y-2': !loading && !disabled
-            }]"
+    :class="[
+      $style.button,
+      { 'hover:opacity-75': !loading && !disabled },
+      { 'bg-deep-purple-400-accent': !loading && !disabled },
+    ]"
     >
       {{ loading ? '' : text }}
+
       <div :class="$style.spinner" v-if="loading"></div>
     </button>
   </div>
@@ -27,6 +29,20 @@ export default {
     text: { type: String, required: true },
     disabled: { type: Boolean, default: false },
   },
+  methods: {
+    clickEvent(e) {
+      this.$emit('clicked');
+      const x = e.clientX - e.target.offsetLeft;
+      const y = e.clientY - e.target.offsetTop;
+      const ripple = document.createElement('span');
+      ripple.style.left = `${x}px`;
+      ripple.style.top = `${y}px`;
+      document.querySelector(`.${this.$style.button}`).appendChild(ripple);
+      setTimeout(() => {
+        ripple.remove();
+      }, 1000);
+    },
+  },
 };
 </script>
 
@@ -34,6 +50,33 @@ export default {
 .button {
   width: 200px;
   height: 45px;
+  position: relative;
+  text-transform: uppercase;
+  cursor: pointer;
+  background-position: center;
+  transition: background 0.8s;
+}
+
+.button span {
+  position: absolute;
+  background: #fff;
+  transform: translate(-50%, -50%);
+  pointer-events: none;
+  border-radius: 50%;
+  animation: animate 0.5s linear forwards;
+}
+
+@keyframes animate {
+  0% {
+    width: 0px;
+    height: 0px;
+    opacity: 0.5;
+  }
+  100% {
+    width: 500px;
+    height: 500px;
+    opacity: 0;
+  }
 }
 
 @keyframes spin {
